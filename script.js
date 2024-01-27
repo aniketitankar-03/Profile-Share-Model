3fc467e026eea4353e9a60a10bab12a6bcc30e6c
aniketitankar <aniketitankar9281@gmail.com>
2024-01-27 11:57:40 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+//   Modal open function
+const openModal = () => {
+  console.log("Modal is Open");
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
+};
+
+// Modal close function
+const closeModal = () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("overlayactive");
+};
+
+function follow(){
+    return "https://www.linkedin.com/in/aniket-itankar-a6b203233"
+}
